03704db7b10ecb04d70523981f4edd125b3ea628
ravikread <ravikread@gmail.com>
2018-09-15 01:03:31 +0530

    fix substring logic

--- common_palindromic_subsequences.py
+++ common_palindromic_subsequences.py
@@ -1,28 +1,59 @@
 import sys
 
+def get_substr_2(input, sub_len):
+	sub_str = []
+	if sub_len == 1:
+		for x in input:
+			sub_str.append(x)
+		#print sub_str
+		return sub_str
+		
+	if (sub_len):
+		nw_input = input[1:]
+		for i in range(0, len(nw_input)):
+			x = input[i: i + 1]
+			sub_str1 = get_substr_2(input[i + 1:], sub_len - 1)
+			'''for sub in sub_str1:
+				sub_str.append(sub)'''
+			for sub in sub_str1:
+				sub_str.append(x + sub)
+	#print sub_str
+	return sub_str
+		
+def get_substr_3(input):
+	sub_str = []
+	for i in range (0, len(input)):
+		tmp_substr = get_substr_2(input, i + 1)
+		for sub in tmp_substr:
+			sub_str.append(sub)
+		
+	return sub_str
+
+def get_substr_1(prx, input):
+	print (prx, input)
+	sub_str = []
+
+	for x in input:
+		n_prx = prx + x 
+		sub_str.append(prx + x)
+		n_sub_str = get_substr_1(n_prx, input[1:])
+		sub_str = sub_str + n_sub_str
+
+	print sub_str
+
+	return sub_str
+		
 
 def get_substr(input):
 	sub_str = []
-	len_str = len(input)
 
-	for i in range (0, len_str):
-		sub_str.append(input[i:i + 1])
-		prfx = input[i : i + 1]
-		for k in range (i + 1, len_str):
-			for j in range (k, len_str):
-				#if prfx + input[k : j + 1] not in sub_str:
-				sub_str.append(prfx + input[k:j + 1])
+	if len(input) == 0:
+		return sub_str
 
-		if i == 0:
-			continue
-
-		prfx = input[0 : i + 1]
-		for k in range (i + 1, len_str):
-			for j in range (k, len_str):
-				#if prfx + input[k : j + 1] not in sub_str:
-				sub_str.append(prfx + input[k:j + 1])
-			
-
+	for i in range(0, len(input)):
+		sub_str.append(input[i : i + 1])
+		print ("start::::")
+		sub_str = sub_str + get_substr_1(input[i : i + 1], input[i + 1:])
 	return sub_str
 
 def revers_str(input):
@@ -40,8 +71,15 @@ def remove_non_poly(sub_str):
 	
 	return poly_str
 
+def remove_dup(sub_str):
+	nw_sub_str = []
+	for sub in sub_str:
+		if sub not in nw_sub_str:
+			nw_sub_str.append(sub)
+	return nw_sub_str
+
 def get_poly(input):
-	sub_str = get_substr(input)
+	sub_str = get_substr_3(input)
 	print (sub_str)
 	poly_str = remove_non_poly(sub_str)
 	print len(poly_str)
@@ -60,7 +98,9 @@ def get_common_poly(input1, input2):
 	poly1 = get_poly(input1)
 	poly2 = get_poly(input2)
 	common_poly = get_common_substr(poly1, poly2)
-	return common_poly
+	nw_sub = remove_dup(common_poly)	
+	#return common_poly
+	return nw_sub
 
 
 
@@ -70,6 +110,7 @@ def main():
 		return 1
 
 	sub_str = get_common_poly(sys.argv[1], sys.argv[2])
+	#sub_str = get_substr_2(sys.argv[1], sys.argv[1], int(sys.argv[2]))
 	print len(sub_str)
 	print sub_str
 
